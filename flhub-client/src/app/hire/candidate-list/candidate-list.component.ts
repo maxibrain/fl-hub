ddea59b1d14592bbe23ac9bab407f9b3a40d5ace
maxibrain <mkostyrkin@gmail.com>
2019-06-04 15:30:49 +0300

    fixes

--- flhub-client/src/app/hire/candidate-list/candidate-list.component.ts
+++ flhub-client/src/app/hire/candidate-list/candidate-list.component.ts
@@ -5,7 +5,7 @@ import { Observable, BehaviorSubject, combineLatest, ReplaySubject } from 'rxjs'
 import { CandidateDto } from '../interfaces/candidate.dto';
 import { ListCandidates, FetchCandidates } from '../state/hire.actions';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { pluck, mergeMap, map, take, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { pluck, mergeMap, map, take, debounceTime, distinctUntilChanged, tap } from 'rxjs/operators';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
@@ -23,6 +23,7 @@ export class CandidateListComponent implements OnInit {
   private readonly _sortings: { [key: string]: (c: CandidateDto) => any } = {
     rate: c => c.profile.rate,
   };
+  readonly search$ = this._search$.asObservable();
   readonly pageIndex$ = this._pagination$.pipe(pluck('pageIndex'));
   readonly pageSize$ = this._pagination$.pipe(pluck('pageSize'));
   readonly candidatesLoading$ = this._candidatesLoading$.asObservable();
@@ -41,12 +42,39 @@ export class CandidateListComponent implements OnInit {
         pluck<Params, string>('name'),
         mergeMap(name => store.select(HireState.candidates(name))),
       ),
-      this._filter$,
+      this._filter$.pipe(
+        tap(filter => {
+          this.router.navigate(['.'], {
+            queryParams: { ...this.route.snapshot.queryParams, ...filter },
+            relativeTo: this.route,
+          });
+        }),
+      ),
       this._search$.pipe(
         debounceTime(500),
         distinctUntilChanged(),
+        tap(phrase => {
+          this.router.navigate(['.'], {
+            queryParams: { ...this.route.snapshot.queryParams, q: phrase || undefined },
+            relativeTo: this.route,
+          });
+        }),
       ),
-      this._sortBy$,
+      this._sortBy$.pipe(
+        tap(sortBy => {
+          if (sortBy) {
+            this.router.navigate(['.'], {
+              queryParams: { ...this.route.snapshot.queryParams, sortBy: sortBy.key, sortDir: sortBy.direction },
+              relativeTo: this.route,
+            });
+          } else {
+            this.router.navigate(['.'], {
+              queryParams: { ...this.route.snapshot.queryParams, sortBy: undefined, sortDir: undefined },
+              relativeTo: this.route,
+            });
+          }
+        }),
+      ),
     ).pipe(
       map(([candidates, filter, search, sortBy]) => {
         candidates = candidates.filter(c => {
@@ -73,10 +101,21 @@ export class CandidateListComponent implements OnInit {
         return candidates;
       }),
     );
-    this.page$ = combineLatest(this.candidates$, this._pagination$).pipe(
-      map(([candidates, pagination]) =>
-        candidates.slice(pagination.pageIndex * pagination.pageSize, (pagination.pageIndex + 1) * pagination.pageSize),
+    this.page$ = combineLatest(
+      this.candidates$,
+      this._pagination$.pipe(
+        tap(({ pageIndex, pageSize }) => {
+          this.router.navigate(['.'], { queryParams: { ...this.route.snapshot.queryParams, pageIndex, pageSize }, relativeTo: this.route });
+        }),
       ),
+    ).pipe(
+      map(([candidates, pagination]) => {
+        const result = candidates.slice(pagination.pageIndex * pagination.pageSize, (pagination.pageIndex + 1) * pagination.pageSize);
+        if (result.length === 0 && pagination.pageIndex > 0) {
+          this._pagination$.next({ pageIndex: Math.floor(candidates.length / pagination.pageSize), pageSize: pagination.pageSize });
+        }
+        return result;
+      }),
     );
   }
 
@@ -90,7 +129,7 @@ export class CandidateListComponent implements OnInit {
       showBad: queryParams['showBad'] === 'true',
     });
     this.onFilterChange();
-    this._search$.next('');
+    this._search$.next(queryParams['q'] || '');
     if (queryParams['sortBy']) {
       this._sortBy$.next({ key: queryParams['sortBy'], direction: queryParams['sortDir'] || 'asc' });
     } else {
@@ -107,12 +146,10 @@ export class CandidateListComponent implements OnInit {
 
   onPageChanged({ pageIndex, pageSize }: any) {
     this._pagination$.next({ pageIndex, pageSize });
-    this.router.navigate(['.'], { queryParams: { ...this.route.snapshot.queryParams, pageIndex, pageSize }, relativeTo: this.route });
   }
 
   onFilterChange() {
     this._filter$.next(this.filterForm.value);
-    this.router.navigate(['.'], { queryParams: { ...this.route.snapshot.queryParams, ...this.filterForm.value }, relativeTo: this.route });
   }
 
   sortBy(sortingKey: string) {
@@ -127,17 +164,9 @@ export class CandidateListComponent implements OnInit {
         )
         .subscribe(e => {
           this._sortBy$.next(e);
-          this.router.navigate(['.'], {
-            queryParams: { ...this.route.snapshot.queryParams, sortBy: e.key, sortDir: e.direction },
-            relativeTo: this.route,
-          });
         });
     } else {
       this._sortBy$.next(null);
-      this.router.navigate(['.'], {
-        queryParams: { ...this.route.snapshot.queryParams, sortBy: undefined, sortDir: undefined },
-        relativeTo: this.route,
-      });
     }
   }
 
